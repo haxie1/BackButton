f3f365c76943f768e7065d9e1a84d0689f90335e
Kam Dahlin <kilodelta@kilodelta-mbp.local>
2016-09-11 07:10:39 -0700

    Initial commit with prep for creating the container view controller

--- BackButton/ContainerViewController.swift
+++ BackButton/ContainerViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ContainerViewController.swift
 //  BackButton
 //
 //  Created by Kam Dahlin on 9/11/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ContainerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
